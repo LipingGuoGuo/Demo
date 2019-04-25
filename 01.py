67201934494eed85ef440afbf27ae7235392b044
2938234632@qq.com <2938234632@qq.com>
2019-04-25 14:35:46 +0800

    link

--- 01.py
+++ 01.py
@@ -3,6 +3,6 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 browser = webdriver.Chrome()
-browser.get('https://www.baidu.com')
+browser.get('https://www.zz-w.cn"')
 browser.find_element_by_id('kw').send_keys('php')
 browser.find_element_by_id('kw').send_keys(Keys.ENTER)
