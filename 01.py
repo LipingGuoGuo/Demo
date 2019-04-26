179d20ee71d4c34b47a1c560258c4c7c045e2b86
2938234632@qq.com <2938234632@qq.com>
2019-04-26 14:58:51 +0800

    try异常练习

--- 01.py
+++ 01.py
@@ -3,6 +3,15 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 browser = webdriver.Chrome()
-browser.get('https://www.zz-w.cn')
+browser.get('http://baidu.com')
 browser.find_element_by_id('kw').send_keys('php')
 browser.find_element_by_id('kw').send_keys(Keys.ENTER)
+
+try:
+    fh = open("01", "w")
+    fh.write("这是一个测试文件，用于测试异常！")
+except IOError:
+    print("Error:没有找到文件或读取文件失败")
+else:
+    print("内容写入文件成功")
+    fh.close()
