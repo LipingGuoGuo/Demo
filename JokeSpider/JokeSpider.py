780bf3a020d23fdd99c219b8e1c0cee5967e1cfa
LipingGuoGuo <2938234632@qq.com>
2019-04-30 17:03:31 +0800

    1.xml解析器报错解决,删除旧的，更新pip3并下载lxml 2.未爬取到内容

--- JokeSpider/JokeSpider.py
+++ JokeSpider/JokeSpider.py
@@ -15,19 +15,19 @@ def creatSoup(url):
     soup_0 = BeautifulSoup(html_text, 'html.parser')
     return soup_0
 
-def creatExcelAndSheet(SheetName):
+def creatExcelAndSheet(sheetName):
     file = xlwt.Workbook(encoding = 'utf-8', style_compression = 0)
-    sheet = file.add_sheet(SheetName)
-    return sheet,file
+    sheet = file.add_sheet(sheetName)
+    return sheet, file
 
-def writeToSheet(a,b,c):
-    sheet.write(a,b,c)
+def writeToSheet(a, b, c):
+    sheet.write(a, b, c)
 
 def summaryAllContent(a,b,url):
     print("提示：抓取结束，无更多内容")
     print("-------------summary----------")
-    print("您抓取的网址为%s"%url)
-    print("共抓取%d页 共%d个内容"%(a-1,b-1))
+    print("您抓取的网址为%s" % url)
+    print("共抓取%d页 共%d个内容" % (a-1, b-1))
     print("------------------------------")
 
 def getEachContent(eachContent):
@@ -45,7 +45,7 @@ k = 1
 while i < 24:
     url = "https://www.qiushibaike.com/8hr/page/" + str(i) + "/?s=4991834"
     soup = creatSoup(url)
-    a_soup = soup.select("a[class=contentHerf]")
+    a_soup = soup.select("a[class=content-block]") # 根据关键字取得按list存放的内容
     contentLen = len(a_soup)
     print("Info:第%d页有%d个笑话" % (i, contentLen))
 
@@ -53,9 +53,13 @@ while i < 24:
         sss = getEachContent(eachContent)
         writeToSheet(k, 0, k)
         writeToSheet(k, 1, sss)
-        print("正在获取第%d个内容...Done" % k)
+        print("正在获取第%d个内容...Done" %k)
         time.sleep(0.05)
         k += 1
+
+    print("提示：正在获取下一页内容...")
+    i += 1
+    time.sleep(3)
 
 summaryAllContent(i, k, url)
 file.save("C:/Users/dell/Desktop/笑话爬虫.xlsx")
