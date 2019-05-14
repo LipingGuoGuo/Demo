16dd78cf6d1b983e43cfd80327897b17569bf65d
2938234632@qq.com <2938234632@qq.com>
2019-05-14 17:48:53 +0800

    feat:爬取最好大学前20排名，fillUnivList(uinfo,html),其中参数输入错误致报错

---
+++ 实例爬取/School_04.py
@@ -0,0 +1,56 @@
+"""
+中国最好大学定向爬虫
+程序的结构设计：
+1.从网络上获取大学排名网页内容
+grtHTMLText()
+2.提取网页内容中信息到合适的数据结构
+fillUnivList()
+3.利用数据结构展示并输出结果
+printUnivList()
+"""
+import requests
+from bs4 import BeautifulSoup
+import bs4
+def getHTMLText(url):
+    try:
+        r = requests.get(url, timeout=30)
+        r.raise_for_status()
+        r.encoding = r.apparent_encoding
+        return r.text
+    except:
+        return ""
+
+def fillUnivList(ulist, html):
+    soup = BeautifulSoup(html, 'lxml')
+    for tr in soup.find('tbody').children:
+        """
+        过滤标签类型
+        """
+        if isinstance(tr, bs4.element.Tag):
+            tds = tr('td')
+            '''
+            .string 取标签的字符串，tr('td') 也就是tr.find_all('td')
+            '''
+            ulist.append([tds[0].string, tds[1].string, tds[2].string])
+
+def printUnivList(ulist, num):
+    print("{:^10}\t{:^6}\t{:^10}".format("排名", "学校名称", "总分"))
+    for i in range(num):
+        u = ulist[i]
+        print("{:^10}\t{:^6}\t{:^10}".format(u[0], u[1], u[2]))
+    # print("Suc" + str(num))
+
+# def main():
+#     uinfo = []
+#     url = "http://www.zuihaodaxue.com/zuihaodaxuepaiming2019.html"
+#     html = getHTMLText(url)
+#     fillUnivList(html, url)
+#     printUnivList(uinfo, 20)  # 20所学校信息
+# main()
+if __name__ == "__main__":
+    uinfo = []
+    url = "http://www.zuihaodaxue.com/zuihaodaxuepaiming2019.html"
+    html = getHTMLText(url)
+    # print(getHTMLText(url))
+    fillUnivList(uinfo, html)
+    printUnivList(uinfo, 20)
